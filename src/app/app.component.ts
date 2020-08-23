56c481dcfa3b073da6c752fc7578d06d3f920aa1
kemora08 <kevin_mora89@hotmail.com>
2020-08-23 12:11:04 -0500

    app.components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { IProduct } from './model/iproduct';
+import { ProductService } from './product-detail/product-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  product: IProduct[] = [];
+
+  constructor(private productService: ProductService,) {}
+
+  ngOnInit(): void{}
+   this.product = this.productService.getProducts();
 }
